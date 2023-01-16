113e06963eb045423d99b4b58a7ee1f6c0634674
Md Ahsanul Hoque <ahrony90@gmail.com>
2023-01-16 14:47:53 -0400

    Add utility resources

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App text-3xl font-bold">
       <div><h2>Simple Blog Filtering/Searching</h2></div>
     </div>
   );
